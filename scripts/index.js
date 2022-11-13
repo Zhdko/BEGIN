cec5434dfd44a2d1b58b358a0dd3eb4745d78891
Alexandr Zhdanko <zhdko@yandex.ru>
2022-11-14 02:47:23 +0400

    feat: add adaptive header light and dark theme

---
+++ scripts/index.js
@@ -0,0 +1,29 @@
+let wriperMenu = document.querySelector('.header__wriper');
+let menu = document.querySelector('.header__menu')
+let header = document.querySelector('.header');
+
+wriperMenu.addEventListener('click', function() {
+  menu.classList.toggle('header__menu_opened');
+});
+
+let toggle = document.querySelector('.toggle');
+let toggleThemeLight = document.querySelector('.toggle__icon_theme_light');
+let toggleThemeDark = document.querySelector('.toggle__icon_theme_dark');
+let handle = document.querySelector('.toggle__ball')
+
+toggle.addEventListener('click', function() {
+  toggleThemeLight.classList.toggle('toggle__icon_active');
+  toggleThemeDark.classList.toggle('toggle__icon_active');
+  handle.classList.toggle('toggle__ball_theme_light');
+
+  if (handle.classList.contains('toggle__ball_theme_light')) {
+    menu.classList.add('header__menu_theme_light');
+    header.classList.add('header_theme_light');
+  }
+  else {
+    menu.classList.remove('header__menu_theme_light');
+    header.classList.remove('header_theme_light');
+  }
+})
+
+
